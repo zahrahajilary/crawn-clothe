86900dd3b2bfc97d2e1d4d55f5382ebbbf35aac8
zahra hajilary <zahra.hajilary.1990@gmail.com>
2020-08-21 15:33:59 +0430

    shoping page added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import {Route, Switch} from 'react-router-dom'
 import './App.css';
-
-const HatsPage = ()=>{
-  return (
-    <div>hats page</div>
-  )
-}
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/hats" exact ={true} component={HatsPage}/>
         <Route path='/' exact={true} component={HomePage}/>
+        <Route path='' exact={true} component={ShopPage}/>
       </Switch>
     </div>
   );
